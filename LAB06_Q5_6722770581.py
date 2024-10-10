a78b90dc6a06bde394a4543fb3d32edc61a7e19e
PattakanA <109189093+PattakanA@users.noreply.github.com>
2024-10-10 19:03:24 +0700

    LAB06_Q5_6722770581.py

--- LAB06_Q5_6722770581.py
+++ LAB06_Q5_6722770581.py
@@ -1,19 +1,15 @@
 a = []
 b = []
 c = []
+h = 97
 num = int(input("how many person in group? : "))
-print("please enter grade from group A")
-for i in range(num):
-    y = float(input("enter grade: "))
-    a.append(y)
-print("please enter grade from group B")
-for i in range(num):
-    y = float(input("enter grade: "))
-    b.append(y)
-print("please enter grade from group C")
-for i in range(num):
-    y = float(input("enter grade: "))
-    c.append(y)
+for x in [a,b,c]:
+    print("please enter grade from group "+chr(h).upper())
+    for i in range(num):
+        y = float(input("enter grade: "))
+        x.append(y)
+    h+=1
+
 print("the highest grade in group A is",max(a))
 print("the highest grade in group B is",max(b))
 print("the highest grade in group C is",max(c))
